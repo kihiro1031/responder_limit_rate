63623ea794e268f992d8a76f20f28db52200d9d1
akihiro <okitwi1000@gmail.com>
2021-06-13 00:29:52 +0900

    feat: rate limit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Overview
+Identify the unique information contained in the header and implement ratelimit.
+
+## Libraries used
+[asgi-ratelimit](https://pypi.org/project/asgi-ratelimit/)
+
+## Try to use
+### application start
+```
+docker compose build
+docker compose up
+```
+
+### request
+Unrestricted endpoints
+```
+curl --location --request GET 'http://localhost:8000/no_limit' \
+--header 'Unique-User-Id: Client A'
+```
+Restricted endpoints
+(Up to once per minute per user)
+```
+curl --location --request GET 'http://localhost:8000/rate_limit' \
+--header 'Unique-User-Id: Client A'
+```
--- src/run.py
+++ src/run.py
@@ -1,12 +1,54 @@
+from typing import Tuple
+
 import responder
+import uvicorn
+from ratelimit import RateLimitMiddleware, Rule
+from ratelimit.auths import EmptyInformation
+from ratelimit.backends.redis import RedisBackend
+from ratelimit.types import Scope
 
 api = responder.API()
 
 
-@api.route("/")
-def hello_world(req, resp):
-    resp.text = "hello, world!"
+async def unique_header_id(scope: Scope) -> Tuple[str, str]:
+    """ Get a unique ID from the header to identify the user. """
+    headers = scope["headers"]
+    user = None
+    for header in headers:
+        if header[0].decode() == "unique-user-id":
+            user = header[1].decode()
+
+    try:
+        return user, "default"
+    except KeyError:
+        raise EmptyInformation(scope)
+
+
+api.add_middleware(
+    RateLimitMiddleware,
+    authenticate=unique_header_id,
+    backend=RedisBackend(host="responder_limit_rate_redis"),
+    config={
+        r"^/rate_limit": [Rule(minute=1, group="default")],
+    },
+)
+
+
+@api.route("/no_limit")
+def no_limit(req, resp):
+    resp.text = "no_limit"
+
+
+@api.route("/rate_limit")
+def rate_limit(req, resp):
+    resp.text = "up to one call per minute."
 
 
 if __name__ == '__main__':
-    api.run()
+    uvicorn.run("run:api",
+                host="0.0.0.0",
+                port=8000,
+                reload=True,
+                log_config='logging.conf',
+                log_level='debug',
+                access_log=False)
